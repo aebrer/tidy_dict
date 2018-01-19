680b81a65ed3f921a501741c282b0502d568afa4
brereton <andrew.brereton@cyclicarx.com>
2018-01-19 12:44:29 -0500

    NOT WORKING

--- nested_dict.py
+++ nested_dict.py
@@ -8,22 +8,7 @@ class NestedDict(dict):
         self[key] = NestedDict()
         return self[key]
 
-    def __str__(self):
-        """
-        Print nicely-ish.
-        :return:
-        """
-        string = "\nNestedDict::{"
-        for key in self.keys():
-            if type(self[key]) == NestedDict:
-                string = string + "\n\n\t" + str(key) + ":"
-                string = string + "\n\t" + str(self[key])
-            else:
-                string = string + "\n\t" + str(key) + ":"
-                string = string + "\t\t" + str(self[key])
-        return string + "}"
-
-    def build_from_tuple(self, key, value=list(), end=True):
+    def build_from_tuple(self, key, value, d=None):
         """
         Build a dictionary from a tuple object directly. Useful if you don't know how long the combinations might be.
         :param key:
@@ -31,24 +16,46 @@ class NestedDict(dict):
         :param end:
         :return:
         """
-        if isinstance(key, tuple):
-            if len(key) >= 2:
-                if key[0] not in self:
-                    self[key[0]] = NestedDict()
-                return self.build_from_tuple(key[0], value, end=False).build_from_tuple(key[1:], value, end=False)
-            if len(key) == 1:
-                return self.build_from_tuple(key[0], value, end=True)
+        # first pass
+        if d is None:
+            if not (isinstance(key, list) or isinstance(key, tuple)):
+                key = [key]
+            key_list = list(key)
+            depth = len(key_list)
+
+            if depth == 1:
+                self[key_list[0]]["."] = value
+
+            else:
+                temp_dict = NestedDict()
+                temp_dict[key_list[0]] = temp_dict.build_from_tuple(key_list[1:], value, temp_dict)
+                all_pairs = list()
+                for keyi in self[key_list[0]]:
+                    all_pairs.append((keyi, self[key_list[0]][keyi]))
+                for keyi in temp_dict:
+                    all_pairs.append((keyi, temp_dict[keyi]))
+                for keyi, value in all_pairs:
+                    self[key_list[0]][keyi] = value
+
         else:
 
-            if isinstance(self[key], NestedDict):
-                if end:
-                    self[key]["."] = value
-                return self[key]
+            key_list = list(key)
+            depth = len(key_list)
+
+            if depth == 1:
+                d[key_list[0]]["."] = value
+
             else:
-                self[key] = NestedDict()
-                if end:
-                    self[key]["."] = value
-                return self[key]
+                temp_dict = NestedDict()
+                temp_dict[key_list[0]] = NestedDict()
+                temp_dict[key_list[0]].build_from_tuple(key_list[1:], value, temp_dict)
+                all_pairs = list()
+                for keyi in d[key_list[0]]:
+                    all_pairs.append((keyi, d[key_list[0]][keyi]))
+                for keyi in temp_dict:
+                    all_pairs.append((keyi, temp_dict[keyi]))
+                for keyi, value in all_pairs:
+                    d[key_list[0]][keyi] = value
 
     def get_from_tuple(self, key):
         """
@@ -96,3 +103,14 @@ class NestedDict(dict):
                         if isinstance(v, NestedDict):
                             for i in d.get_from_tuple_iter(key_list[1:], v):
                                 yield i
+
+
+test = NestedDict()
+test.build_from_tuple((1),[1424])
+print test
+test.build_from_tuple((1,2,2),[1424])
+print test
+
+test.build_from_tuple((1,4,2),[1424])
+
+print test
