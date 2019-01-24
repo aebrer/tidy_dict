91ce723715e9c00acb1f5a343b02a3072b402392
atomoton <andrew@brereton.me>
2019-01-24 10:30:10 -0500

    update print statements by @DevonWelch

--- nested_dict.py
+++ nested_dict.py
@@ -3,14 +3,26 @@ class NestedDict(dict):
         self[key] = NestedDict()
         return self[key]
 
-    def __str__(self):
-        string = "\nNestedDict::{"
+    def __str__(self, t=1):
+        if len(self.keys()) == 0:
+            if t == 1:
+                t = 0
+            return "\t"*(t-1) + "{Empty NestedDict}"
+        elif t == 1:
+            string = "NestedDict::{"
+        else:
+            string ="\t"*(t-1) + "{"
 
-        for key in self.keys():
+        for key in list(self.keys()):
+
             if type(self[key]) == NestedDict:
-                string = string + "\n\n\t" + str(key) + ":"
-                string = string + "\n\t" + str(self[key])
+                string = string + "\n\n" + "\t"*t + str(key) + ":"
+                string = string + "\n" + "\t"*t + self[key].__str__(t+1)
             else:
-                string = string + "\n\t" + str(key) + ":"
+                string = string + "\n" + "\t"*t + str(key) + ":"
                 string = string + "\t\t" + str(self[key])
-        return string + "}"
+
+        if t == 1:
+            t = 0
+        return string + "\n" + "\t"*t + "}"
+
