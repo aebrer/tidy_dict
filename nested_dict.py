b412f0ba6ae2f400f7bd6a0e172ceb74c7a0cccf
atomoton <andrew@brereton.me>
2019-02-19 10:32:56 -0500

    more updates to nested dict type from @DevonWelch

--- nested_dict.py
+++ nested_dict.py
@@ -1,9 +1,14 @@
+"""
+nested dictionary class, for easy sparse matrices
+"""
+
 class NestedDict(dict):
     def __missing__(self, key):
         self[key] = NestedDict()
         return self[key]
 
     def __str__(self, t=1):
+        '''outputs an easily readable string representation'''
         if len(self.keys()) == 0:
             if t == 1:
                 t = 0
@@ -25,4 +30,30 @@ class NestedDict(dict):
         if t == 1:
             t = 0
         return string + "\n" + "\t"*t + "}"
+
+    def __repr__(self):
+        '''outputs an accurate string representation that you can initialize an exact copy from'''
+        if len(self.keys()) == 0:
+            return "NestedDict()"
+        else:
+            string = "NestedDict("
+
+        for key in list(self.keys()):
+
+            if type(self[key]) == NestedDict:
+                string += f"{key} = {self[key].__repr__()},"
+            else:
+                string += f"{key} = {self[key]},"
+
+        return f"{string})"
+
+    def to_dict(self):
+        '''convert to a normal dict'''
+        new_dict = {}
+        for key, val in self.items():
+            if type(val) == NestedDict:
+                new_dict[key] = val.to_dict()
+            else:
+                new_dict[key] = val
+        return new_dict
 
