b2d6f50710d9a2a7f6e66196b8fb5da7d161042d
atomoton <andrew@brereton.me>
2019-07-17 11:52:46 -0400

    ready to package

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
-from setuptools import setup
-from setuptools import find_packages
+import setuptools
 
-setup(name='NestedDict',
-      description='A Nested Dictionary Class',
-      long_description='A dictionary that can be used to easily make nested dictionaries and export to CSV as tidy data',
-      version='1.0.0',
-      url='https://github.com/atomoton/nested_dict',
-      license='gpl-3.0',
-      packages=find_packages('src'),
-      package_dir={'': 'src'},
-      zip_safe=False
-      )
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nested_dict",
+    version="1.0.0",
+    author="Andrew E Brereton",
+    author_email="andrew@brereton.me",
+    description="A tool for easy Nested Dictionaries with option to output tidy CSV files.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/atomoton/nested_dict",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GPL License",
+        "Operating System :: OS Independent",
+    ],
+)
